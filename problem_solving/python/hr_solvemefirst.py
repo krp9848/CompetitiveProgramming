84d457df33098d991d23b9661746ec1ef2c6f192
kabiraj <kabirajpant@gmail.com>
2020-03-14 22:37:54 +0530

    hr-solvemefirst problem solved~linux

---
+++ problem_solving/python/hr_solvemefirst.py
@@ -0,0 +1,14 @@
+
+
+def solveMeFirst(a,b):
+    """
+    return the sum of the given two numbers
+    """
+    return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
+
