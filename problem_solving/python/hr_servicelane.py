cf57cb5d58aeb6ca391202ec3e804951e92cb0c0
krp9848 <kabirajpant@gmail.com>
2020-07-03 01:21:14 +0530

    hr_service_lane problem implemented

---
+++ problem_solving/python/hr_servicelane.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the serviceLane function below.
+def serviceLane(n, cases):
+    return [min(width[case[0]:case[1] + 1]) for case in cases]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nt = input().split()
+
+    n = int(nt[0])
+
+    t = int(nt[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(n, cases)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
