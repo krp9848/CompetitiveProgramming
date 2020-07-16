959c1b546ae5ac872aabe04ed5b9a0fabd572b30
krp9848 <kabirajpant@gmail.com>
2020-07-16 23:38:19 +0530

    hr birthday chocolate implementation

---
+++ problem_solving/python/hr_birthday_choco.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthday function below.
+def birthday(s, d, m):
+
+    return sum([True if sum(s[i:i+m]) == d else False for i in range(len(s) - m + 1)])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    dm = input().rstrip().split()
+
+    d = int(dm[0])
+
+    m = int(dm[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
